5fceed85799b22ef746631d1504c1b8ecea4979d
Manswi Sati <manvisati03@gmail.com>
2026-02-10 17:40:22 +0530

    Add maxArea method optimally to calculate water container area
    Implement maxArea method to calculate the maximum water area.

---
+++ con_with_most_water/Optimal/water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height)-1
+        maxArea = 0
+
+
+        while(left < right):
+            width = right-left
+            curr_height = min(height[left], height[right])
+            area = width*curr_height
+
+            maxArea = max(area,maxArea)
+
+            if (height[left] < height[right]):
+                left +=1
+            else:
+                right -=1
+
+        
+        return maxArea
